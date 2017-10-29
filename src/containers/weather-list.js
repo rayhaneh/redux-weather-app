94887012668726042e5ea7571ae917893bbe9f17
Rayhaneh <r.b.assady@gmail.com>
2017-10-29 19:11:50 -0400

    Added react-sparklines and created a reusable chart component.

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name
+    const temp = cityData.list.map( (l) => (l.main.temp));
+    const pres = cityData.list.map( (l) => (l.main.pressure));
+    const humi = cityData.list.map( (l) => (l.main.humidity));
     return (
       <tr key={name}>
         <td>
           {name}
+        </td>
+        <td>
+          <Chart data={temp} color='red' />
+        </td>
+        <td>
+          <Chart data={pres} color='blue' />
+        </td>
+        <td>
+          <Chart data={humi} color='orange' />
         </td>
       </tr>
     )
