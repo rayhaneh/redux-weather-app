bde0779d399cda277c9842d2947e1f6a7927b62b
Rayhaneh <r.b.assady@gmail.com>
2017-10-29 14:48:04 -0400

    Added state to searchBar container (container state not redux state)

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      term: '' 
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  };
+
+  onInputChange(e) {
+    this.setState({term: e.target.value});
+  }
+
   render() {
     return (
       <form className='input-group'>
-        <input />
+        <input 
+          placeholder='Get a five day forcast in your favorite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className='input-group-btn'>
-          <button type='submit' className='btn btn-secondary'>Submit</button>
+          <button type='submit' className='btn btn-secondary'>Submitg</button>
         </span>
       </form>
     )
   }
-}
+};
 
 export default SearchBar;
